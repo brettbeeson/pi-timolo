aabf02a92aaa3c1d789f573f2496092c53448064
brettbeeson <brettbeeson@fastmail.com>
2019-10-18 16:23:57 +1000

    X

--- source/filebyday.sh
+++ source/filebyday.sh
@@ -5,10 +5,17 @@
 # Requires "daily-photos" and "photos" folders to be correct
 # Updates "quick-links" todo!
 
+# If true, cp (or cp -r) to quick-links, instead of ln -s
+# Required for file-systems without symblinks (eg. s3)
+
+#
+# !!! use python AWS S3 sdk or cli to mv instead of sync, mv, sync !!!
+# or modify pi-timolo to put in daily-photos directly! (better)
+#
 copy_not_link=1
 
 function photo_hours_ago() {
-	rm $quick_links_dir/latest-$1h.jpg 1>/dev/null 2>&1 || true
+	rm -rf "$quick_links_dir"/latest-$1h.jpg 1>/dev/null 2>&1 || true
 	FILENAME_MATCH=$(date -d "-$1 hours" +"%Y%m%d-%H")
 	PHOTO=$(find daily-photos -name *$FILENAME_MATCH*.jpg -type f | sort -n | tail -n 1)
 	if [ ! -z "$PHOTO" ]; then
@@ -18,7 +25,6 @@ function photo_hours_ago() {
 			ln -s ../$PHOTO $quick_links_dir/latest-$1h.jpg
 		fi	
 	fi
-
 }
 
 if [ -z "$1" ]; then
@@ -31,6 +37,9 @@ if [ ! -d $1 ]; then
 fi
 
 cd $1
+
+# pull latest photos
+aws s3 sync s3://tmv.brettbeeson.com.au/picam/photos photos
 
 filebyday=/usr/local/bin/filebyday.py
 
@@ -57,3 +66,10 @@ photo_hours_ago 06
 photo_hours_ago 09
 photo_hours_ago 12
 photo_hours_ago 24
+
+# push changes to photos (removes old)
+aws s3 sync --delete photos s3://tmv.brettbeeson.com.au/picam/photos
+# push changes to daily-photos (files, no remove required)
+aws s3 sync daily-photos s3://tmv.brettbeeson.com.au/picam/daily-photos
+# push changes to quick-links (removes old)
+aws s3 sync --delete quick-links s3://tmv.brettbeeson.com.au/picam/quick-links
