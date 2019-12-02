af874174ab50fb209664e8de7e80a1d8f07cf76d
brettbeeson <brettbeeson@fastmail.com>
2019-12-02 17:49:38 +1000

    Update from working pitlc

--- source/makedailymovies.sh
+++ source/makedailymovies.sh
@@ -1,43 +1,16 @@
-#!/bin/bash
+#!/bin/bash 
 #
 # Search the dailyphotos directory for days (e.g. 2011-01-11, 2011-01-12) with jpg in them
 # If required, make a video of these and save it as 2011-01-11*.mp4 in dailyvideos
-##
-# $1: basey! directory
 #
-# File Structure:
-# ---------------
-# root
-# |-- daily-videos
-# |---- 2019-01-02.mp4           	<-- create these
-# |-- daily-photos 			<-- search here for isoformat dates
-# |---- 2019-01-02
-# |------- 2019-01-01T10-10-10.jpg	<--- search for jpg to make videos
-# |------- 2019-01-01T10-10-20.jpg
-
-run_dashify=true
+#
+# $1: dailyphotos folder
+# $2: dailyvideos folder
 
 TLMM=/usr/local/bin/tlmm.py
-DASHIFY=/usr/local/bin/dashify.sh
 
-if [ -z "$1" ]; then
-	echo "No argument supplied"
-	exit 3
-fi
-if [ ! -d "$1" ]; then
-	echo "No such directory: $1" 1>&2
-	exit 2
-fi
-
-cam=$(basename "$1")
-
-# Use relative where possible
-# After a cd, use $ROOTDIR/$dailyphotos
-cd "$1" || exit 1
-ROOTDIR=$(pwd)
-dailyphotos=daily-photos
-dailyvideos=daily-videos
-new_videos=0
+dailyphotos=${1:-"daily-photos"}
+dailyvideos=${2:-"daily-videos"}
 
 if [ ! -d "$dailyvideos" ]; then
 	echo "$dailyvideos: No such directory"
@@ -48,68 +21,47 @@ if [ ! -d "$dailyphotos" ]; then
 	exit 1
 fi
 
-tmpdir=$(mktemp -d)
-echo $(date --iso-8601=minutes): Starting makedailymovies.sh
-echo Tmp: $tmpdir
-echo Root: $ROOTDIR
-echo Photos: $dailyphotos
-echo Videos: $dailyvideos
-echo Cam: $cam
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # get cur dir of this script
+progName=$(basename -- "$0")
+cd $DIR
 
-echo Pulling latest photos
-# --show-only-errors
-aws s3 sync s3://tmv.brettbeeson.com.au/"$cam"/daily-photos daily-photos
-echo Pulling latest daily-videos. This is so when we upload with --delete, we can remove the right ones
-aws s3 sync s3://tmv.brettbeeson.com.au/"$cam"/daily-videos daily-videos
+tmpdir=`mktemp`
+
 
 # For each folder of a day's photos...
-for d in "$dailyphotos"/*/; do
-	# $dailyphotos is an absolute path, do d is too
-	cd $ROOTDIR/$d
-	day=$(basename $d)
-
-	nfiles=$(ls -l *.jpg 2>/dev/null | grep -v ^d | grep -v ^t | wc -l)
-	[ -z $nfiles ] && nfiles=0
-	if [ -f nfiles ]; then
-		lastnfiles=$(<nfiles)
+for d in $dailyphotos/*/ ; do
+    day=$(basename $d)
+    nfiles=$( ls -l $d*.jpg 2> /dev/null | grep -v ^d | grep -v ^t | wc -l )
+    [ -z "$nfiles" ] &&  nfiles=0
+    if [ -f $d/nfiles ]; then
+    	lastnfiles=$(<$d/nfiles)
+    else
+    	lastnfiles=0
+    fi
+    #[ -z "$lastnfiles" ] &&  lastnfiles=0
+    nmovies=$( ls -l $dailyvideos/$day*.mp4 2> /dev/null | grep -v ^d | grep -v ^t | wc -l )
+    #echo $d: Found $nfiles images \(previously $lastnfiles\) and $nmovies movies.
+    # Only run if more files available, or no movies
+    if [ $nfiles -gt $lastnfiles -o $nmovies -eq 0 ]; then
+    	echo $d: Making a movie
+	# Move away old existing movies for that day (might have different hour-suffixs
+  	mv $dailyvideos/$day*.mp4 $tmpdir 2>/dev/null
+	# cwd to dir so tmp files and logs are in the relevant place
+	cd "$DIR"/"$d"
+	"$TLMM" video --log-level DEBUG --dest "$DIR/$dailyvideos" "$DIR/$d*.jpg" > "$DIR/$d/tlmm.log" 2>&1
+	cd "$DIR"
+	mademovie=$?
+	if [ $mademovie -eq 0 ]; then
+           echo $nfiles > "$d/nfiles"
+	   rm $tmpdir/$day*.mp4 2>/dev/null
 	else
-		lastnfiles=0
+	   echo $d: Make movie failed: code $mademovie
+	   # Move them back
+	   mv $tmpdir/$day.mp4  $dailyvideos/
 	fi
-	nvideos=$(ls -l $ROOTDIR/$dailyvideos/$day*.mp4 2>/dev/null | grep -v ^d | grep -v ^t | wc -l)
-	echo "$day": Found $nfiles images \(previously $lastnfiles\) and $nvideos videos.
-	# Only run if more files available, or no videos
-	if [ $nfiles -gt $lastnfiles -o $nvideos -eq 0 ]; then
-		echo $day: Making a video
-		# Move away old existing videos and dash dirs for that day (might have different hour-suffixs
-		mv $ROOTDIR/$dailyvideos/$day*.mp4 $tmpdir/ 2>/dev/null
-		mv $ROOTDIR/$dailyvideos/$day* $tmpdir/ 2>/dev/null
-		# cwd to dir so tmp files and logs are in the relevant place
-		video_made=$($TLMM video --log-level DEBUG --force --dest $ROOTDIR/$dailyvideos *.jpg 2>tlmm.log)
-		madevideo=$?
-		if [ $madevideo -eq 0 ]; then
-			echo $nfiles >nfiles
-			rm $tmpdir/$day*.mp4 2>/dev/null || true
-			rm $tmpdir/$day* 2>/dev/null || true
-			# Rename from 2019-01-01T15_to_2019-01-01T19.mp4 to 2019-01-01.mp4
-			mv "$video_made" "$ROOTDIR"/$dailyvideos/"$day".mp4
-			video_made="$ROOTDIR"/$dailyvideos/"$day".mp4
-			if $run_dashify; then
-				echo Dashify video: $video_made
-				dash_dir=$($DASHIFY "$video_made" $ROOTDIR/$dailyvideos/) || echo Error: dashify "$video_made" "$ROOTDIR/$d"
-			fi
-			let new_videos++
-		else
-			echo $day: Make video failed: code $madevideo. Check $(pwd)/tlmm.log
-			# Move them back
-			mv "$tmpdir"/"$day".mp4 "$ROOTDIR"/$dailyvideos/
-			mv "$tmpdir"/"$day"* "$ROOTDIR"/$dailyvideos/
-			
-		fi
-	fi
-	rm -f "$tmpdir"/* > /dev/null 2>&1
-	rmdir "$tmpdir" > /dev/null 2>&1
+    else 
+	echo $d: Movie not required
+    fi
+# could delete tmpdir
 done
 
-echo Syncing changes
-# push changes to daily-movies (files, overwrites daily movies from early in the day)
-aws s3 sync "$ROOTDIR"/"$dailyvideos" s3://tmv.brettbeeson.com.au/"$cam"/daily-videos
--- source/makelongermovies.sh
+++ source/makelongermovies.sh
@@ -1,101 +1,64 @@
-#!/bin/bash 
+#!/bin/bash
 #
-# Take the daily videos and combine them in to last week, last month and last year videos
+# Take the daily movies and combine them in to last week, last month and last year movies
 #
-# $1: root directory
-#
-# File Structure:
-# ---------------
-# root
-# |-- daily-videos
-# |---- 2019-01-02T10.mp4           <-- input
-# |---- 2019-01-03T10.mp4           <-- input
-# |-- longer-videos
-# |---- last-2days.mp4           	<-- output
-# |---- last-weeks					<-- output
-
-run_dashify=true
-
-# Concat, dashify and quick-link
-# $1 : from: a past time (eg "1 week ago")
-# $2 : to: a past time (eg "1 week ago")
-# $3 : quick link name (eg "today-video")
-# $4 : relative speed
-function make_video() {
-	#													warning: quote confusion attack! seperate files
-	concat_video=$($TLCONCAT --speed-rel "$4" "$1" "$2" ${daily_videos[*]})
-	if [ $? -eq 0 ]; then
-		mv "$concat_video" "$3".mp4
-		concat_video="$3".mp4
-		if $run_dashify; then
-			echo Dashify video: "$concat_video"
-			dash_dir=$($DASHIFY "$concat_video" .) || echo Error: dashify of "$concat_video" 1>&2
-		fi
-	fi
-}
-
-if [ -z "$1" ]; then
-	echo "No argument supplied"
-	exit 3
-fi
-if [ ! -d "$1" ]; then
-	echo "No such directory: $1" 1>&2
-	exit 2
-fi
-cam=$(basename "$1")
-
+# $1: daily_videos_dir folder (input)
+# $2: longer_videos_dir folder (output)
+#set -o xtrace
 TLCONCAT=/usr/local/bin/tlconcat.py
-DASHIFY=/usr/local/bin/dashify.sh
 
-daily_videos_dir="$1"/daily-videos
-longer_videos_dir="$1"/longer-videos
-quick_links_dir="$1"/quick-links
-# abs to allow cd later
+daily_videos_dir=${1:-"picam/daily-videos"}
+longer_videos_dir=${2:-"picam/longer-videos"}
+
+# move to script's dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # get cur dir of this script
+progName=$(basename -- "$0")
+cd $DIR
+
+
+# cd to put log files in right place. then must use absolute paths for arguments
 daily_videos_dir=$(realpath $daily_videos_dir)
+daily_videos=($daily_videos_dir/*.mp4)
 longer_videos_dir=$(realpath $longer_videos_dir)
-quick_links_dir=$(realpath $quick_links_dir)
-daily_videos=($daily_videos_dir/20*.mp4)
 
 if [ ! -d "$daily_videos_dir" ]; then
 	echo "$daily_videos_dir: No such directory"
 	exit 1
 fi
 if [ ! -d "$longer_videos_dir" ]; then
-	mkdir "$longer_videos_dir"
-	if [ ! -d "$longer_videos_dir" ]; then
-		echo "$longer_videos_dir: No such directory"
-		exit 1
+  mkdir "$longer_videos_dir"
+  if [ ! -d "$longer_videos_dir" ]; then
+	  echo "$longer_videos_dir: No such directory"
+	  exit 1
 	fi
 fi
 
-echo $(date --iso-8601=minutes): Starting $0 
-echo Pulling latest daily-videos. Usually we have them locally, so this shouldnt run except on first/restart/etc.
-aws s3 sync s3://tmv.brettbeeson.com.au/"$cam"/daily-videos daily-videos
+cd $longer_videos_dir
 
-# Only run if there are daily videos newer the long-video
-# BUT: in S3, directories don't really exist!
-# So use the oldest file in each directory instead to signify directory age
-oldest_daily_video=$(ls -tr  "$daily_videos_dir"/*.mp4 | tail -1)
-oldest_longer_video=$(ls -tr  "$longer_videos_dir"/*.mp4 | tail -1)
 
-# There are some longer-videos already
-if [ ! -z "$oldest_longer_video" ]; then
-	# And they are newer than the daily videos  	
-	if [ "$oldest_daily_video" -ot "$oldest_longer_video" ]; then
-		echo Nothing to do. "$daily_videos_dir" is older than "$longer_videos_dir".
-		exit 0
-	fi
+
+#set -o xtrace # print commands for logging
+# TLCONCAT outputs the resultant filename
+last_2_days_movie="$($TLCONCAT  --speed-rel 1 'yesterday' 'today' ${daily_videos[*]})"
+if [ $? -eq 0 ]; then
+	echo last_2_days_movie = $last_2_days_movie
+	mv $last_2_days_movie A-last_two_days_$last_2_days_movie || true
+fi
+last_week_movie="$($TLCONCAT  --speed-rel 5 '2 days ago' 'today' ${daily_videos[*]})"
+if [ $? -eq 0 ]; then
+	mv $last_week_movie B-last_week_$last_week_movie || true
 fi
 
-# tlconcat writes to cwd.
-cd "$longer_videos_dir" || exit 1
+last_month_movie="$($TLCONCAT  --speed-rel 10 '30 days ago' today ${daily_videos[*]})"
+if [ $? -eq 0 ]; then
+	mv $last_month_movie C-last_month_$last_month_movie || true
+fi
 
-# Make new videos by concating daily-videos, dashify and add quick-links
-make_video 'yesterday'  	'yesterday' 'yesterday-video'	1
-make_video '1 week ago' 	'today'     'weekly-video'    	10
-make_video '1 month ago' 	'today' 	'monthly-video' 	30
-make_video '5 years ago' 	'today' 	'complete-video' 	100
+# hack for "forever"
+complete_movie="$($TLCONCAT  --speed-rel 100 '100 years ago' today ${daily_videos[*]})"
+if [ $? -eq 0 ]; then
+	mv $complete_movie D-complete_$complete_movie || true
+fi
+set +o xtrace
 
-# push new movie/s; no need to delete as the same names such as "today.mp4"
-aws s3 sync --only-show-errors "$longer_videos_dir" s3://tmv.brettbeeson.com.au/"$cam"/longer-videos
-exit 0
+
